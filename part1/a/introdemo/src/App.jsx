3556e24c16bbdaa025cff20f6977c4f54183530e
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-06-23 15:15:05 +0200

    INTRODEMO (D): Handling Arrays (the concat method returns a new array with item appended)

--- part1/a/introdemo/src/App.jsx
+++ part1/a/introdemo/src/App.jsx
@@ -1,26 +1,35 @@
 import { useState } from "react";
 
-
-const Display = ({counter}) => <div>{counter}</div>;
-
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-
 const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const [counter, setCounter] = useState(0);
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"))
+    setLeft(left + 1)
+  }
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"))
+    setRight(right + 1)
+  }
+
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button text="plus" onClick={increaseByOne}/>
-      <Button text="zero" onClick={setToZero}/>
-      <Button text="minus" onClick={decreaseByOne}/>
+      {left}
+      <button onClick={handleLeftClick}>
+        left
+      </button>
+      <button onClick={handleRightClick}>
+        right
+      </button>
+      {right}
+      <p>{allClicks.join(" ")}</p>
     </div>
-  );
+  )
 }
 
 export default App;
