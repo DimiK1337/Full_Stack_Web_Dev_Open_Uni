9e15b248e26fb3c98c7e77f6116868ad896beaa4
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-06-23 15:21:52 +0200

    INTRODEMO (D): Update of the state is async

--- part1/a/introdemo/src/App.jsx
+++ part1/a/introdemo/src/App.jsx
@@ -4,16 +4,21 @@ const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat("L"))
-    setLeft(left + 1)
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
   }
 
 
   const handleRightClick = () => {
     setAll(allClicks.concat("R"))
-    setRight(right + 1)
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
   }
 
 
@@ -28,6 +33,7 @@ const App = () => {
       </button>
       {right}
       <p>{allClicks.join(" ")}</p>
+      <p>Total Clicks: {total}</p>
     </div>
   )
 }
