d97e9c7ddc1de6a5846a85aa399a404bae90a4ee
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-06-23 18:36:43 +0200

    2.6: The Phonebook Step 1

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Dimitriy Kruglikov' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+
+
+  const addName = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+  }
+  
+  const handleNameChange = (event) => setNewName(event.target.value)
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      { persons.map(person => <li key={person.name}>{person.name}</li>) }
+    </div>
+  )
+}
+
+export default App
