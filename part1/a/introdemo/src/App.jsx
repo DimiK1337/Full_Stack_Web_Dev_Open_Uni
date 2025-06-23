2551bf5643f5739a7a1ac5df53c2f893ad28c63c
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-06-23 14:55:58 +0200

    INTRODEMO: Event handlers

--- part1/a/introdemo/src/App.jsx
+++ part1/a/introdemo/src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 
 const App = () => {
-  
+
   const [counter, setCounter] = useState(0);
 
-  /**
-   * When setCounter is called, the whole component re-renders, meaning that the counter is increased every second. This behaves like setInterval, where a delay is given to re-execute code. 
-   */
-  setTimeout(() => {
-    setCounter(counter + 1);
-  }, 1000);
 
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+      <button onClick={() => setCounter(0)}>
+        zero
+      </button>
+    </div>
   );
 }
 
