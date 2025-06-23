bfed0fa47a01dfb5cd632b733c299185077bf397
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-06-23 14:51:29 +0200

    INTRODEMO: Stateful components

--- part1/a/introdemo/src/App.jsx
+++ part1/a/introdemo/src/App.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 
-const App = (props) => {
-  const { counter } = props;
+const App = () => {
+  
+  const [counter, setCounter] = useState(0);
+
+  /**
+   * When setCounter is called, the whole component re-renders, meaning that the counter is increased every second. This behaves like setInterval, where a delay is given to re-execute code. 
+   */
+  setTimeout(() => {
+    setCounter(counter + 1);
+  }, 1000);
 
   return (
     <div>{counter}</div>
--- part1/a/introdemo/src/main.jsx
+++ part1/a/introdemo/src/main.jsx
@@ -1,16 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-let counter = 1;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const refresh = () => {
-    root.render(
-        <App counter={counter} />
-    );
-}
-
-setInterval(() => {
-    refresh();
-    counter += 1;
-}, 1000);
+root.render(
+    <App />
+);
