1cba8ef22834fb690d17201babb3f2fbc3c55983
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-06-25 18:01:36 +0200

    BACKEND: Removed CORS from index.js and package.json

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
 
-const cors = require('cors');
+
 let notes = [
     { id: "1", content: "HTML is easy", important: true },
     { id: "2", content: "Browser can execute only JavaScript", important: false },
     { id: "3", content: "GET and POST are the most important methods of HTTP protocol", important: true }
 ];
-
-app.use(cors()); // Enable CORS for all routes
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
