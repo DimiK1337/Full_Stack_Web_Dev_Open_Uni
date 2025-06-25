3b8a59446ff33020127458b2e5facb05b283af7f
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-06-25 16:31:22 +0200

    3.8*: Phonebook backend step 8

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -29,7 +29,11 @@ let persons = [
 // MIDDLEWARE
 app.use(express.json());
 
-app.use(morgan("tiny"));
+morgan.token("body", (req) => {
+    return JSON.stringify(req.body);
+});
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
+
 
 // ENDPOINTS
 
