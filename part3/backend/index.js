6115984953e481f9aeeed68cfa59d8d5aa317a17
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-06-27 15:06:26 +0200

    BACKEND: Removed the dummy notes

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -2,13 +2,6 @@ require('dotenv').config() // Load environment variables from .env file
 const express = require('express')
 const app = express()
 
-
-let notes = [
-  { id: '1', content: 'HTML is easy', important: true },
-  { id: '2', content: 'Browser can execute only JavaScript', important: false },
-  { id: '3', content: 'GET and POST are the most important methods of HTTP protocol', important: true }
-]
-
 app.use(express.json()) // Middleware to parse JSON bodies
 
 const morgan = require('morgan')
