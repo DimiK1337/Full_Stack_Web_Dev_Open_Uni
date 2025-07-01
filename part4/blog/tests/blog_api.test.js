384201c89b4800f727a05d8bf81dfb3f64b95162
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-07-01 15:38:40 +0200

    4.23*: Blog List Expansion, step 11

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -52,8 +52,7 @@ describe('when there are some initial blogs saved', () => {
     })
   })
 
-  // POST
-  describe('when adding a blog to the db', () => {
+  describe('when making requests with JWT auth', () => {
     let token
     beforeEach(async () => {
       await User.deleteMany({})
@@ -71,102 +70,132 @@ describe('when there are some initial blogs saved', () => {
       token = loginRes.body.token
     })
 
-    test('it will be added to the database', async () => {
-      const newBlog = {
-        title: 'Is Java the tenth circle of hell? Screw over your colleague and suggest it for the frontend',
-        author: 'Tori Black',
-        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-        likes: 10
-      }
+    // POST
+    describe('when adding a blog to the db', () => {
+      test('it will be added to the database', async () => {
+        const newBlog = {
+          title: 'Is Java the tenth circle of hell? Screw over your colleague and suggest it for the frontend',
+          author: 'Tori Black',
+          url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+          likes: 10
+        }
 
-      await api
-        .post('/api/blogs')
-        .set('Authorization', `Bearer ${token}`)
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+        await api
+          .post('/api/blogs')
+          .set('Authorization', `Bearer ${token}`)
+          .send(newBlog)
+          .expect(201)
+          .expect('Content-Type', /application\/json/)
 
-      // Get the blogs from the db and confirm the newly added blog is there
-      const blogs = await helper.blogsInDB()
-      assert(blogs.length, helper.listWithManyBlogs.length + 1)
+        // Get the blogs from the db and confirm the newly added blog is there
+        const blogs = await helper.blogsInDB()
+        assert(blogs.length, helper.listWithManyBlogs.length + 1)
 
-      // Check if the title of the new blog is in the returned blogs
-      const titles = blogs.map(blog => blog.title)
-      assert(titles.includes(newBlog.title))
+        // Check if the title of the new blog is in the returned blogs
+        const titles = blogs.map(blog => blog.title)
+        assert(titles.includes(newBlog.title))
+      })
+
+      test('a blog with the missing `likes` property will default to 0', async () => {
+        const newBlog = {
+          title: 'Is Java the tenth circle of hell? Screw over your colleague and suggest it for the frontend',
+          author: 'Tori Black',
+          url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        }
+
+        await api
+          .post('/api/blogs')
+          .set('Authorization', `Bearer ${token}`)
+          .send(newBlog)
+          .expect(201)
+          .expect('Content-Type', /application\/json/)
+
+        const blogs = await helper.blogsInDB()
+        const mostRecentBlog = blogs[blogs.length - 1]
+
+        assert(Object.hasOwn(mostRecentBlog, 'likes'))
+        assert.strictEqual(mostRecentBlog.likes, 0)
+      })
+
+      test('a blog missing the `title` property will send a 400 BAD REQUEST', async () => {
+        const newBlog = {
+          author: 'Tori Black',
+          url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf'
+        }
+
+        const res = await api
+          .post('/api/blogs')
+          .set('Authorization', `Bearer ${token}`)
+          .send(newBlog)
+          .expect(400)
+          .expect('Content-Type', /application\/json/)
+
+        assert(res.error.text.includes('Missing title from blog'))
+      })
+
+      test('a blog missing the `url` property will send a 400 BAD REQUEST', async () => {
+        const newBlog = {
+          title: 'Is Java the tenth circle of hell? Screw over your colleague and suggest it for the frontend',
+          author: 'Tori Black'
+        }
+
+        const res = await api
+          .post('/api/blogs')
+          .set('Authorization', `Bearer ${token}`)
+          .send(newBlog)
+          .expect(400)
+          .expect('Content-Type', /application\/json/)
+
+        assert(res.error.text.includes('Missing url from blog'))
+      })
+
+      test('a blog without the auth token will yield a 401', async () => {
+        const newBlog = {
+          title: 'Is Java the tenth circle of hell? Screw over your colleague and suggest it for the frontend',
+          author: 'Tori Black',
+          url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        }
+
+        await api
+          .post('/api/blogs')
+          .set('Authorization', 'Bearer INVALID_TOKEN')
+          .send(newBlog)
+          .expect(401)
+          .expect('Content-Type', /application\/json/)
+      })
     })
 
-    test('a blog with the missing `likes` property will default to 0', async () => {
-      const newBlog = {
-        title: 'Is Java the tenth circle of hell? Screw over your colleague and suggest it for the frontend',
-        author: 'Tori Black',
-        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-      }
+    // DELETE
+    describe('when deleting a blog from the db', () => {
+      test('a status code of 204 is returned', async () => {
+        const blogsAtStart = await helper.blogsInDB()
 
-      await api
-        .post('/api/blogs')
-        .set('Authorization', `Bearer ${token}`)
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+        const newBlog = { ...blogsAtStart[0], title: 'this will be deleted' }
 
-      const blogs = await helper.blogsInDB()
-      const mostRecentBlog = blogs[blogs.length - 1]
+        // Post the blog so the proper user can delete it
+        const res = await api
+          .post('/api/blogs')
+          .set('Authorization', `Bearer ${token}`)
+          .send(newBlog)
+          .expect(201)
 
-      assert(Object.hasOwn(mostRecentBlog, 'likes'))
-      assert.strictEqual(mostRecentBlog.likes, 0)
-    })
+        const blogToDelete = res.body
 
-    test('a blog missing the `title` property will send a 400 BAD REQUEST', async () => {
-      const newBlog = {
-        author: 'Tori Black',
-        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf'
-      }
+        // Delete the newBlog through it's id and compare the length
+        await api
+          .delete(`/api/blogs/${blogToDelete.id}`)
+          .set('Authorization', `Bearer ${token}`)
+          .expect(204)
 
-      const res = await api
-        .post('/api/blogs')
-        .set('Authorization', `Bearer ${token}`)
-        .send(newBlog)
-        .expect(400)
-        .expect('Content-Type', /application\/json/)
+        const blogsAtEnd = await helper.blogsInDB()
+        assert(blogsAtEnd.length, helper.listWithManyBlogs.length)
 
-      assert(res.error.text.includes('Missing title from blog'))
-    })
-
-    test('a blog missing the `url` property will send a 400 BAD REQUEST', async () => {
-      const newBlog = {
-        title: 'Is Java the tenth circle of hell? Screw over your colleague and suggest it for the frontend',
-        author: 'Tori Black'
-      }
-
-      const res = await api
-        .post('/api/blogs')
-        .set('Authorization', `Bearer ${token}`)
-        .send(newBlog)
-        .expect(400)
-        .expect('Content-Type', /application\/json/)
-
-      assert(res.error.text.includes('Missing url from blog'))
+        const titles = blogsAtEnd.map(blog => blog.title)
+        assert(!titles.includes(blogToDelete.title))
+      })
     })
   })
 
-  // DELETE
-  describe('when deleting a blog from the db', () => {
-    test('a status code of 204 is returned', async () => {
-      const blogsAtStart = await helper.blogsInDB()
-      const blogToDelete = blogsAtStart[0]
-
-      // Delete the newBlog through it's id and compare the length
-      await api
-        .delete(`/api/blogs/${blogToDelete.id}`)
-        .expect(204)
-
-      const blogsAtEnd = await helper.blogsInDB()
-      assert(blogsAtEnd.length, helper.listWithManyBlogs.length - 1)
-
-      const titles = blogsAtEnd.map(blog => blog.title)
-      assert(!titles.includes(blogToDelete.title))
-    })
-  })
 
   // PUT
   describe('when updating a blog from the db', () => {
