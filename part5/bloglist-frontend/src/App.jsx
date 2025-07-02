e1c208d7b9c3119a9dc25c45e388e4cf095968aa
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-07-02 16:55:39 +0200

    5.10: Blog List Frontend, step 10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -98,7 +98,7 @@ const App = () => {
         
         <br />
         {
-          blogs.map(blog =>
+          blogs.sort((a,b) => b.likes - a.likes).map(blog =>
             <Blog 
               key={blog.id} 
               blog={blog} 
