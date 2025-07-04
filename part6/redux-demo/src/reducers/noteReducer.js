f91405119b1f9771991f92c5d94795609826f6ae
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-07-04 17:38:20 +0200

    PART 6.A: Forwarding the store to many components

--- part6/redux-demo/src/reducers/noteReducer.js
+++ part6/redux-demo/src/reducers/noteReducer.js
@@ -1,4 +1,4 @@
-import { legacy_createStore as createStore } from 'redux'
+
 const noteReducer = (state = [], action) => {
   switch (action.type) {
     case 'NEW_NOTE': return [...state, action.payload]
@@ -15,37 +15,17 @@ const noteReducer = (state = [], action) => {
   }
 }
 
-const store = createStore(noteReducer)
+const generateId = () => Number((Math.random() * 1000000).toFixed(0))
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
+export const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    payload: { content, important: false, id: generateId() }
   }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
-
-const App = () => {
-  return (
-    <div>
-      <ul>
-        {store.getState().map(note =>
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-      </ul>
-    </div>
-  )
 }
+
+export const toggleImportanceOf = (id) => {
+  return { type: 'TOGGLE_IMPORTANCE', payload: { id } }
+}
+
 export default noteReducer;
