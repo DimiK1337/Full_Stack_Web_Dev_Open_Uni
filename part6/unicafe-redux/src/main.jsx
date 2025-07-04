40ce5f9a6610fdb7ac344248400f6404841735ef
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-07-04 16:49:13 +0200

    6.2: Unicafe Revisited, step2

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -1,27 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { createStore } from 'redux'
+import { legacy_createStore as createStore} from 'redux'
 import reducer from './reducer'
 
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
+  
+  const clickDispatch = review => store.dispatch({ type: review })
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => clickDispatch('GOOD')}>good</button>
+      <button onClick={() => clickDispatch('OK')}>ok</button>
+      <button onClick={() => clickDispatch('BAD')}>bad</button>
+      <button onClick={() => clickDispatch('ZERO')}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
