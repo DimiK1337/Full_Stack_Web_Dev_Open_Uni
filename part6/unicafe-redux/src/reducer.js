5e600ce1a580b97daa948bb0501c1c10e00586a1
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-07-04 17:57:03 +0200

    6.3: Anecdotes, step 1

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -7,18 +7,12 @@ const initialState = {
 const counterReducer = (state = initialState, action) => {
   console.log("action at the top", action)
   switch (action.type) {
-    case 'GOOD': {
-      return { ...state, good: state.good + 1 }
-    }
-    case 'OK':
-      return { ...state, ok: state.ok + 1 }
-    case 'BAD':
-      return { ...state, bad: state.bad + 1 }
-    case 'ZERO':
-      return initialState
+    case 'GOOD': return { ...state, good: state.good + 1 }
+    case 'OK': return { ...state, ok: state.ok + 1 }
+    case 'BAD': return { ...state, bad: state.bad + 1 }
+    case 'ZERO': return initialState
     default: return state
-  }
-  
+  } 
 }
 
 export default counterReducer
