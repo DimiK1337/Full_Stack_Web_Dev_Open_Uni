7b0a9c7a9f09ea8bfad9c60a8e7741c94e70d700
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-07-07 17:24:52 +0200

    PART 6.C: Redux thunk & Async actions

--- part6/redux-demo/src/main.jsx
+++ part6/redux-demo/src/main.jsx
@@ -1,23 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import { configureStore } from  '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
-
-import filterReducer from './reducers/filterReducer'
-import noteReducer from './reducers/noteReducer'
-
+import store from './store'
 import App from './App'
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer
-  }
-})
-
-console.log(store.getState())
-store.subscribe(() => console.log(store.getState()))
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
--- part6/redux-demo/src/reducers/noteReducer.js
+++ part6/redux-demo/src/reducers/noteReducer.js
@@ -1,30 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const initialState = [
-  {
-    content: 'reducer defines how redux store works',
-    important: true,
-    id: 1,
-  },
-  {
-    content: 'state of store can contain any data',
-    important: false,
-    id: 2,
-  },
-]
-
-const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+import noteService from '../services/notes'
 
 const noteSlice = createSlice({
   name: 'notes',
   initialState: [],
   reducers: {
-
-    createNote(state, action) {
-      const newNoteObject = action.payload
-      return state.concat(newNoteObject)
-    },
-
     toggleImportanceOf(state, action) {
       const id = action.payload
       const noteToChange = state.find(n => n.id === id)
@@ -47,5 +28,19 @@ const noteSlice = createSlice({
   }
 })
 
-export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+export const { toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
 export default noteSlice.reducer
+
+export const initializeNotes = () => {
+  return async dispatch => {
+    const notes = await noteService.getAll()
+    dispatch(setNotes(notes))
+  }
+}
+
+export const createNote = content => {
+  return async dispatch => {
+    const newNote = await noteService.createNote(content)
+    dispatch(appendNote(newNote))
+  }
+}
