662010dcc11e69d958da2edb68b77d98d944c769
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-07-09 23:16:54 +0200

    PART 7 EXERCISES: Added `store.js` to bloglist-redux to improve readability

--- part7/bloglist-redux/bloglist-frontend/src/main.jsx
+++ part7/bloglist-redux/bloglist-frontend/src/main.jsx
@@ -1,22 +1,10 @@
 import ReactDOM from 'react-dom/client'
-
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
-
-import notificationReducer from './reducers/notificationReducer'
-import blogReducer from './reducers/blogReducer'
-import userReducer from './reducers/userReducer'
 
 import App from './App'
 import './index.css'
 
-const store = configureStore({
-  reducer: {
-    notification: notificationReducer,
-    blogs: blogReducer,
-    user: userReducer
-  }
-})
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
