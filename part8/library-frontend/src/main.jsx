5ee892f6bbf2e4da53739630f3e6a4255a9837f6
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-07-16 17:55:56 +0200

    8.8: Authors view

---
+++ part8/library-frontend/src/main.jsx
@@ -0,0 +1,19 @@
+import ReactDom from 'react-dom/client'
+import App from './App'
+
+import { 
+  ApolloClient, 
+  ApolloProvider,
+  InMemoryCache, 
+} from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache()
+})
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>
+)
