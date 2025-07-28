549b9311cc8a678b7b3e62399933d02ec56ff4a2
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-07-28 18:06:27 +0200

    9.3 Command line

--- part9/exercise-projects/bmiCalculator.ts
+++ part9/exercise-projects/bmiCalculator.ts
@@ -1,3 +1,16 @@
+import { isNotNumber } from "./utils"
+
+interface CalculateBMIValues {
+  height: number,
+  weight: number
+}
+
+const parseArgs = (args: string[]): CalculateBMIValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+  if (isNotNumber(args[2]) || isNotNumber(args[3])) throw new Error('Provided values were not numbers')
+  return { height: Number(args[2]), weight: Number(args[3]) }
+}
 
 /**
  * 
@@ -6,18 +19,28 @@
  * @returns A string with the BMI category
  */
 const calculateBmi = (height: number, weight: number): string => {
-    const heightInMeters = height / 100
-    const bmi = (weight) / (heightInMeters * heightInMeters)
-    console.log('bmi=', bmi)
+  const heightInMeters = height / 100
+  const bmi = (weight) / (heightInMeters * heightInMeters)
+  console.log('bmi=', bmi)
 
-    if (bmi < 16) return "Underweight (Severe thinness)";
-    else if (bmi < 17) return "Underweight (Moderate thinness)";
-    else if (bmi < 18.5) return "Underweight (Mild thinness)";
-    else if (bmi < 25) return "Normal range";
-    else if (bmi < 30) return "Overweight (Pre-obese)";
-    else if (bmi < 35) return "Obese (Class I)";
-    else if (bmi < 40) return "Obese (Class II)";
-    return "Obese (Class III)";
+  if (bmi < 16) return "Underweight (Severe thinness)";
+  else if (bmi < 17) return "Underweight (Moderate thinness)";
+  else if (bmi < 18.5) return "Underweight (Mild thinness)";
+  else if (bmi < 25) return "Normal range";
+  else if (bmi < 30) return "Overweight (Pre-obese)";
+  else if (bmi < 35) return "Obese (Class I)";
+  else if (bmi < 40) return "Obese (Class II)";
+  return "Obese (Class III)";
 }
 
-console.log(calculateBmi(180, 74))
+try {
+  const { height, weight } = parseArgs(process.argv)
+  const res = calculateBmi(height, weight)
+  console.log(res)
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened. '
+  if (error instanceof Error) {
+    errorMessage += error.message
+  }
+  console.log(errorMessage)
+}
--- part9/exercise-projects/exerciseCalculator.ts
+++ part9/exercise-projects/exerciseCalculator.ts
@@ -1,3 +1,5 @@
+import { isNotNumber } from "./utils";
+
 type Rating = 1 | 2 | 3
 
 interface Result {
@@ -10,8 +12,23 @@ interface Result {
   average: number;
 }
 
+interface CalculateExerciseValues {
+  weekly_hours: number[];
+  target: number;
+}
+
+
+const parseArgs = (args: string[]): CalculateExerciseValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  return {
+    target: Number(args[2]),
+    weekly_hours: args.slice(3).map(Number)
+  }
+}
+
+
 const calculateExercises = (weekly_hours: number[], target: number): Result => {
-  const average =  weekly_hours.reduce((acc, cur) => acc + cur, 0) / weekly_hours.length;
+  const average = weekly_hours.reduce((acc, cur) => acc + cur, 0) / weekly_hours.length;
 
   let rating: Rating = 1;
   let ratingDescription: string = "You need to work much harder!";
@@ -26,16 +43,26 @@ const calculateExercises = (weekly_hours: number[], target: number): Result => {
   }
 
   return {
+    success: average >= target,
     periodLength: weekly_hours.length,
     trainingDays: weekly_hours.filter(h => h !== 0).length,
     target,
     average,
-    success: average >= target,
     rating,
     ratingDescription
   }
 }
 
 
-const res = calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2);
-console.log('res', res);
+try {
+  const { weekly_hours, target } = parseArgs(process.argv);
+  const res = calculateExercises(weekly_hours, target);
+  console.log(res);
+}
+catch (error: unknown) {
+  let errorMessage = 'Something bad happened: ';
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
