a01f38b2f8477cf559fbc0b026bcb330f6b0a068
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2025-07-29 14:23:16 +0200

    9.9: Patientor backend, step2

--- part9/patientor/backend/index.ts
+++ part9/patientor/backend/index.ts
@@ -1,13 +1,20 @@
 import express from 'express';
+import cors, { CorsOptions } from 'cors';
 
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+const corsOptions: CorsOptions = {
+  origin: 'http://localhost:5173'
+};
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors(corsOptions));
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
+
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on  http://localhost:${PORT}`);
